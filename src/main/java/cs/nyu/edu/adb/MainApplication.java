9e77b259a1623941030d19b494901ff16ce7c59e
DerrickBu <15843126572@163.com>
2019-11-27 13:10:49 -0500

    Run all the tests if there's no input

--- src/main/java/cs/nyu/edu/adb/MainApplication.java
+++ src/main/java/cs/nyu/edu/adb/MainApplication.java
@@ -7,21 +7,36 @@ public class MainApplication {
   public static void main(String[] args) {
 
     if(args.length == 0) {
-      throw new IllegalArgumentException("Should provide a filename");
-    }
+      for (int i = 1; i < 12; i++) {
+        // Parse input file, get a list of operations
+        IOUtils ioUtils = new IOUtils();
+        IOUtils.inputFile = String.format("./TestInput/test%s.txt", i);
+        try {
+          IOUtils.createOutputFile();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        ioUtils.parseFile();
 
-    // Parse input file, get a list of operations
-    IOUtils ioUtils = new IOUtils();
-    IOUtils.inputFile = args[0];
-    try {
-      IOUtils.createOutputFile();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    ioUtils.parseFile();
+        // send all parsed operations to transaction manager, and start running
+        TransactionManager transactionManager = new TransactionManager(ioUtils.operations);
+        transactionManager.run();
+      }
+    } else {
 
-    // send all parsed operations to transaction manager, and start running
-    TransactionManager transactionManager = new TransactionManager(ioUtils.operations);
-    transactionManager.run();
+      // Parse input file, get a list of operations
+      IOUtils ioUtils = new IOUtils();
+      IOUtils.inputFile = args[0];
+      try {
+        IOUtils.createOutputFile();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      ioUtils.parseFile();
+
+      // send all parsed operations to transaction manager, and start running
+      TransactionManager transactionManager = new TransactionManager(ioUtils.operations);
+      transactionManager.run();
+    }
   }
 }
